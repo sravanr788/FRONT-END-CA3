055ae7b215448665a3c01adf04774c555976a066
sravanr788 <sravanr7882gmail.com>
2023-12-01 18:38:37 +0530

    done

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 let url = "https://www.themealdb.com/api/json/v1/1/random.php"
+// fetching a random meal from the API and displaying its details
 
 async function getRandomMeal(){
     try{
         let res = await axios.get(url)
         let data = await res.data
         console.log(data)
+        // extracting details of the first meal from the response
         let meal = data.meals[0]
         console.log()
         console.log(meal.strMealThumb)
+        // Creating HTML content for the random meal and displaying it in 'card' element
         let randommealdetails = `<div>
         <img src="${meal.strMealThumb}" alt="" class="randomealimg">
     </div>
     <p>${meal.strMeal}</p>`
     document.getElementById("card").innerHTML=randommealdetails
-
+       // Function to open a modal with ingredients when the card of random meal is clicked
     function openModal() {
         document.getElementById('popup').style.display = 'flex';   
         let ingredients = `<img src="${meal.strMealThumb}" alt="" class="popupimage">
         <p class="popuptext">`;
-
+            // Looping through ingredients and measures and adding them to the modal content
         for (let i=1;i<20;i++) {
-            // let ingredient = meal.strIngredient
             let ingredient = meal[`strIngredient${i}`];
             let measure = meal[`strMeasure${i}`];
-            // Checining  if both strIngredient and strMeasure are not empty
+            // Checking  if both strIngredient and strMeasure are not empty
             if (ingredient && measure) {
                 ingredients += `${ingredient}: ${measure} <br>`;
             }
@@ -32,7 +34,7 @@ async function getRandomMeal(){
         ingredients+=`</p>`
         document.getElementById("modalcontent").innerHTML = ingredients
     }
-
+      // Attaching the 'openModal' function when  random meal is clicked
     let randomMealCard = document.getElementById("card")
     randomMealCard.onclick=()=>{
         openModal()
@@ -40,6 +42,7 @@ async function getRandomMeal(){
     
     }
     catch{(err)=>{
+        // handling any errors that occur during the process    
         console.error("Error:",err)
     }
 }
@@ -47,21 +50,24 @@ async function getRandomMeal(){
 getRandomMeal() 
 
 const url1 = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
-
+// function to get meals by search value and displaying them
 async function getMealBySearch(value) {
     try {
+        // making the search URL based on the user's input
         let url2 = url1 + `${value}`;
         let res = await fetch(url2);
         let data = await res.json();
 
         let allMeals = data.meals;
         let output = "";
-
-        if(allMeals==null){
+// Checking if no meals are found and displaying a message
+        if(allMeals === null){
             document.getElementById("categories").style.display="block"
-            document.getElementById("categories").innerHTML = "No Such Results Found.Please try another";
+            document.getElementById("categories").innerHTML = "No Such Results Found.Please try again with another dish";
         }
-        else {
+        else if(allMeals!=null){
+   // generating HTML content for each search result and displaying in 'categories' element
+            document.getElementById("categories").style.display="grid"
         for (let i = 0; i < allMeals.length; i++) {
             let meal = allMeals[i];
             output += `<div class="card" onclick="openSearchModal(${i})">
@@ -69,7 +75,7 @@ async function getMealBySearch(value) {
                 <p>${meal.strMeal}</p>
             </div>`;
 
-            // Append modal content for each search result meal
+            // Appending modal content for each search result meal
             output += `<div id="searchModal${i}" class="modal">
                 <div class="modal-content">
                 <div class="modal-header flex">
@@ -80,6 +86,7 @@ async function getMealBySearch(value) {
                     <img src="${meal.strMealThumb}" alt="" class="popupimage">
                     <p class="popuptext">`;
 
+       // Loop through ingredients and measures and add them to the modal content
             for (let j = 1; j < 20; j++) {
                 let ingredient = meal[`strIngredient${j}`];
                 let measure = meal[`strMeasure${j}`];
@@ -94,9 +101,11 @@ async function getMealBySearch(value) {
                 </div>
             </div>`;
         }
-        
+        // Displaying the search results in the 'categories' element
         document.getElementById("categories").innerHTML = output;
     }
+
+        // Focusing on 'categories' and scroll it into view smoothly on search
         document.getElementById("categories").focus()
         document.getElementById("categories").scrollIntoView({
             behavior:'smooth',
@@ -105,23 +114,24 @@ async function getMealBySearch(value) {
 
 
     } catch (err) {
+       //to Handle any errors that occur during the process
         console.error("Error:", err);
     }
 }
 
-// Open modal for a specific search result meal
+// function to open modal for a specific search result meal
 function openSearchModal(index) {
     document.getElementById(`searchModal${index}`).style.display = 'flex';
 }
 
-// Close modal for a specific search result meal
+//function to  close modal for a specific search result meal
 function closeSearchModal(index) {
     document.getElementById(`searchModal${index}`).style.display = 'none';
 }
 
 document.getElementById("Searchbox").addEventListener("keypress", (e) => {
     let value = e.target.value;
-
+      // Checking if Enter key is pressed and the input value is not empty
     if (e.key === 'Enter' && value!=='') {
         getMealBySearch(value);
         }
@@ -130,7 +140,7 @@ document.getElementById("Searchbox").addEventListener("keypress", (e) => {
     }
 });
 
-
+// function to close the modal
 function closeModal() {
     document.getElementById('popup').style.display = 'none';
 }
